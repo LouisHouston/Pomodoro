389657c15d4c882c8803b22bf886516c136b554d
LouisHouston <69780196+LouisHouston@users.noreply.github.com>
2022-05-26 13:10:58 -0700

    Update pomoro.py

--- pomoro.py
+++ pomoro.py
@@ -8,23 +8,23 @@ class counter():
     
     def __init__(self):
         self.root = Tk()
-        self.label = Tk.Label(self.root, text = "Text")
         self.ttl_seconds = 0
-        self.frm = Tk.grid()
-        self.startbutton = Tk.Button(self.frm, text="Start", command=self.startPomo).grid(column=1, row=0)
-        self.quitbutton = Tk.Button(self.frm, text='Quit', command=self.quit).grid(column=2, row = 0)
-      
+        self.frm = Tk.grid(self.root)
+        self.label = ttk.Label(self.root, text= self.ttl_seconds,font = 'Arial 40 bold', foreground= 'cyan').grid(column=0 , row=0)
+        self.startbutton = ttk.Button(self.frm, text="Start", command=self.startPomo).grid(column=1, row=0)
+        self.quitbutton = ttk.Button(self.frm, text='Quit', command=self.quit).grid(column=2, row = 0)
+    
         self.root.geometry("250x70")
         self.root.title("Pomodoro")
         self.frame = ttk.Frame(self.root, padding =10)
-        self.root.mainloop
+        self.root.mainloop()
 
     def startPomo(self):
         self.ttl_seconds = 1500
         while self.ttl_seconds > 0:
             timer = datetime.timedelta(seconds = self.ttl_seconds)
-            root.update()
-            root.after(1000)
+            self.root.update()
+            self.root.after(1000)
             print('something')
             self.ttl_seconds -= 1
             (self.ttl_seconds)
@@ -32,13 +32,16 @@ class counter():
     def quit(self):
         self.root.destroy()
 
-  
+
             
+
 
     def stopPomo():
         ttl_seconds = 25 * 60
         
 
+
+print("Fre")
 
 
 #root = Tk()
